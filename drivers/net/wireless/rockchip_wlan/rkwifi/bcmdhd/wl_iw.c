c7b9c864d1e6a1bdd7a12be3eff855bde9c12a62
robodan <DanChristian65@gmail.com>
2018-06-11 10:16:39 -0700

    Fix conflicting blocking structure
    Blocking and indentation were inconsistent for Japanese channel handling.

--- drivers/net/wireless/rockchip_wlan/rkwifi/bcmdhd/wl_iw.c
+++ drivers/net/wireless/rockchip_wlan/rkwifi/bcmdhd/wl_iw.c
@@ -601,8 +601,9 @@ wl_iw_set_freq(
 			while (fwrq->e++ < 6)
 				fwrq->m /= 10;
 		}
+	}
 	/* handle 4.9GHz frequencies as Japan 4 GHz based channelization */
-	if (fwrq->m > 4000 && fwrq->m < 5000)
+	if (fwrq->m > 4000 && fwrq->m < 5000) {
 		sf = WF_CHAN_FACTOR_4_G; /* start factor for 4 GHz */
 
 		chan = wf_mhz2channel(fwrq->m, sf);
